be9a1b9e3114b4c95b2d385ec95a7dc022aa9d55
aftern0on <faa31.31.31.31@mail.ru>
2024-12-20 05:20:43 +0300

    изменение настроек бд

--- auth/app/framework/database.py
+++ auth/app/framework/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession as _AsyncSession
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/auth")
 engine = create_async_engine(DATABASE_URL, future=True)
 AsyncSession = sessionmaker(class_=_AsyncSession, autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
